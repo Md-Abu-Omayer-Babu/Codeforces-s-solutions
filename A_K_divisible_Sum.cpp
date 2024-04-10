af32e11894010daf6863f70dd08afd1013d9a67f
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-04-10 10:31:18 +0600

    k_divisible_sum

---
+++ A_K_divisible_Sum.cpp
@@ -0,0 +1,36 @@
+// Problem link : https://codeforces.com/problemset/problem/1476/A
+
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+int32_t main(){
+    tc{
+        int n, k;
+        cin >> n >> k;
+
+        if(n > k){
+            int temp = k;
+            k *= (n / k);
+
+            if(k < n){
+                k += temp;
+            }
+            
+        }
+
+        int ans = ceil(double(k) / double(n));
+
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
