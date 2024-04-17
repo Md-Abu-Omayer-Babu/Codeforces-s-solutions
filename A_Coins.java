ab69e9bd24cf66ceaf7a306c7f1d920cbe3fb9ab
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-04-17 17:49:32 +0600

    Add files via upload

---
+++ A_Coins.java
@@ -0,0 +1,25 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+import java.util.Scanner;
+
+public class A_Coins {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        long t = scn.nextLong();
+        while (t > 0){
+            long n = scn.nextLong();
+            long k = scn.nextLong();
+
+            if((n % 2 == 0) || (n - k) % 2 == 0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+
+            t--;
+        }
+    }
+}
