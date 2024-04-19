11ce4fd2249403446079b4d54d562f30501e67f9
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-04-19 21:43:09 +0600

    Add files via upload

---
+++ A_Entertainment_in_MAC.cpp
@@ -0,0 +1,28 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+int32_t main(){
+    tc{
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        string str1 = s;
+        string str3 = s + s;
+        reverse(s.begin(), s.end());
+        string str2 = s + str1;
+
+        cout << min(str1, min(str2, str3)) << endl;
+    }
+
+    return 0;
+}
