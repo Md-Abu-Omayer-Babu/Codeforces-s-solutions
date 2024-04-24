b51ce14eae72d9276bee8db777fb6c834c80c3c4
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-04-24 10:39:58 +0600

    README.md

--- README.py
+++
@@ -1,5 +0,0 @@
-print(Codeforces Problem Solutions Repository\n")
-
-print("Welcome to my Codeforces problem solutions repository! Here you'll find my solutions to various Codeforces problems, organized by their respective problem codes and titles. Whether you're a beginner or an experienced coder, this repository aims to provide clear and concise solutions to help you understand and tackle programming challenges efficiently.\n")
-
-print("Thank You.....")
