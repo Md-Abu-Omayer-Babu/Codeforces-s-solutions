e3aabfd561bef0b39463d26d7ddd19eda25d4447
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-04-29 14:38:01 +0600

    Multiplication_Table

---
+++ A_Multiplication_Table.cpp
@@ -0,0 +1,27 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+int32_t main(){
+    int n, x;
+    cin >> n >> x;
+
+    int count = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(x % i == 0 && x <= i * n){
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
