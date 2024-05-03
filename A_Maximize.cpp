a0828878b892032bcb0a7638bbcb6aef0ba1dc07
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-03 12:06:50 +0600

    Maximize

---
+++ A_Maximize.cpp
@@ -0,0 +1,26 @@
+// Problem link : https://codeforces.com/contest/1968/problem/A
+
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    tc {
+        int n;
+        cin >> n;
+        
+        cout << n - 1 << endl;
+    }
+
+    return 0;
+}
