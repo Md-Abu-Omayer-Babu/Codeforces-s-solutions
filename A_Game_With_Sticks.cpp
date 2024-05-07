375db07260d8008a75ddabf9da9dbcb3942cdfcd
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-07 18:42:52 +0600

    Game_With_Sticks

---
+++ A_Game_With_Sticks.cpp
@@ -0,0 +1,25 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define endl "\n"
+
+int32_t main(){
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+
+        if(min(n,k) % 2 == 0){
+            cout << "Malvika" << endl;
+        }else{
+            cout << "Akshat" << endl;
+        }
+        
+    }
+
+    return 0;
+}
