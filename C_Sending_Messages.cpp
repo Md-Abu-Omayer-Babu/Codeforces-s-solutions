2a4fb3c45599c9d84d6261b69d0052067a58f9c2
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-15 23:19:35 +0600

    Sending_Messages

---
+++ C_Sending_Messages.cpp
@@ -0,0 +1,31 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long int
+
+int32_t main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, f, a, b;
+        cin >> n >> f >> a >> b;
+        
+        vector<int> v(n + 1);
+        v[0] = 0;
+        for(int i = 1; i <= n; i++){
+            cin >> v[i];
+        }
+
+        for(int i = 1; i <= n; i++){
+            f -= min((v[i] - v[i - 1]) * a, b);
+        }
+
+        f > 0 ? puts("YES") : puts("NO");
+
+    }
+
+    return 0;
+}
