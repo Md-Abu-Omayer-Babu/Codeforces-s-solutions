97bb911f55879f82f7028756dbb701a2be8173f0
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-17 16:12:32 +0600

    Beautiful_Array

--- B_Beautiful_Array.cpp
+++ B_Beautiful_Array.cpp
@@ -13,25 +13,25 @@ using namespace std;
 
 void solve(){
     int n, k, b, s;
-        cin >> n >> k >> b >> s;
+    cin >> n >> k >> b >> s;
 
-        vector <int> a(n);
+    vector <int> a(n);
 
-        a[0] = k * b;
-        s -= k * b;
-        if (s < 0) cout << "-1\n";
-        else {
-            for (int i = 0; i < n; ++i) {
-                int now = min(k - 1, s);
-                a[i] += now;
-                s -= now;
-            }
-            if (s > 0) cout << "-1\n";
-            else {
-                for (int i = 0; i < n; ++i) cout << a[i] << " ";
-                cout << "\n";
-            }
+    a[0] = k * b;
+    s -= k * b;
+    if (s < 0) cout << "-1\n";
+    else {
+        for (int i = 0; i < n; ++i) {
+            int now = min(k - 1, s);
+            a[i] += now;
+            s -= now;
         }
+        if (s > 0) cout << "-1\n";
+        else {
+            for (int i = 0; i < n; ++i) cout << a[i] << " ";
+            cout << "\n";
+        }
+    }
 }
 
 int32_t main() {
