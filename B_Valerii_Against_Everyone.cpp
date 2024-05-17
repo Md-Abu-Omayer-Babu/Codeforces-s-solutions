c56194d308d73bb6e7dffbe893f2520544a85b6e
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-17 22:36:23 +0600

    Hints : Just Check Duplicate Occurence

---
+++ B_Valerii_Against_Everyone.cpp
@@ -0,0 +1,39 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int n;
+    cin >> n;
+
+    bool duplicate = false;
+    map<int, int> mp;
+    for(auto i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        mp[x]++;
+        if(mp[x] > 1){
+            duplicate = true;
+        }
+    }
+
+    duplicate ? yes : no;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    tc{
+        solve();
+    }
+
+    return 0;
+}
