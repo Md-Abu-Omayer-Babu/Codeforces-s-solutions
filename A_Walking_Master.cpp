213cd8bf93e261c3124ffca1078f3b32607d5345
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-05-22 20:48:42 +0600

    Walking_Master

---
+++ A_Walking_Master.cpp
@@ -0,0 +1,35 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if(b <= d && c <= a + b - d){
+        int move = (d - b) + (a + d - b - c);
+        cout << move << endl;
+        return;
+    }
+    
+    cout << - 1 << endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    tc {
+        solve();
+    }
+
+    return 0;
+}
