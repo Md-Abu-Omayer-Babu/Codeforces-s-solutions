2691d58a5552a0e1ab301ee7e8d79878a4d4f1b0
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-06-07 09:14:29 +0600

    XOR_Sequences

---
+++ B_XOR_Sequences.cpp
@@ -0,0 +1,35 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+
+void solve(){
+    int x, y;
+    cin >> x >> y;
+
+    int XOR = x ^ y;
+
+    int length = 1;
+    
+    while(XOR % 2 == 0){
+        XOR >>= 1;
+        length <<= 1;
+    }
+
+    cout << length << endl;
+}
+
+int32_t main(){
+    tc{
+        solve();
+    }
+
+    return 0;
+}
