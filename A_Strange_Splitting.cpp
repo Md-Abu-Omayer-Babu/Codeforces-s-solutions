2edc0f2d50b6a3e4a51947751ca95edb2e7a17ea
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-06-10 15:52:14 +0600

    Strange_Splitting

---
+++ A_Strange_Splitting.cpp
@@ -0,0 +1,23 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+
+void solve(){
+    
+}
+
+int32_t main(){
+    tc{
+        solve();
+    }
+
+    return 0;
+}
