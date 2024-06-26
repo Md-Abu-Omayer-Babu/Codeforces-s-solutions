123eefc279da5734ad6b8b20e36dc17db4abf3a0
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-06-26 14:36:37 +0600

    Guess_the_Maximum

--- A_Guess_the_Maximum.cpp
+++ A_Guess_the_Maximum.cpp
@@ -5,27 +5,34 @@
 using namespace std;
 #define endl "\n"
 #define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 #define tc int t;cin >> t;while(t--)
+
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(auto &it : v){
+        cin >> it;
+    }
+
+    int maxi = 0;
+    int ans = INT_MAX;
+
+    for(int i = 0; i < n - 1; i++){
+        maxi = max(v[i], v[i + 1]);
+        ans = min(maxi, ans);
+    }
+
+    cout << ans - 1 << endl;
+}
 
 int32_t main(){
     tc{
-        int n;
-        cin >> n;
-
-        vector<int> v(n);
-        for(auto &it : v){
-            cin >> it;
-        }
-
-        int maxi = 0;
-        int ans = INT_MAX;
-
-        for(int i = 0; i < n - 1; i++){
-            maxi = max(v[i], v[i + 1]);
-            ans = min(maxi, ans);
-        }
-
-        cout << ans - 1 << endl;
+        solve();
     }
 
     return 0;
