360c4fd9ce19e7934d983415ac5df0b92fac0928
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-07-08 16:40:42 +0600

    Array_Divisibility

---
+++ A_Array_Divisibility.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int32_t main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        
+        for(int i = 1; i <= n; i++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
