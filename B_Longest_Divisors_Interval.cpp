9876101b49f927e768661c2c3cf8dbcbda3f05e3
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-07-09 23:30:29 +0600

    Longest_Divisors_Interval

---
+++ B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,40 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    int temp = 0;
+
+    for(int i = 1; i <= n; i++){
+        cerr << i << " ";
+        if(n % i == 0){
+            cnt++;
+        }else{
+            break;
+        }
+    }
+    
+    cout << cnt << endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    tc{
+        solve();
+    }
+
+    return 0;
+}
