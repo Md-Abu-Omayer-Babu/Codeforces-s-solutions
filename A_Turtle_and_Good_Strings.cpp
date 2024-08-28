9784895f568e713ea0d30f8cd068e4f085dc81fd
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-08-28 18:57:18 +0600

    hint : compare first and last characters

---
+++ A_Turtle_and_Good_Strings.cpp
@@ -0,0 +1,36 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    if(s[0] == s[n - 1]){
+        no;
+        return;
+    }
+
+    yes;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    tc{
+        solve();
+    }
+
+    return 0;
+}
