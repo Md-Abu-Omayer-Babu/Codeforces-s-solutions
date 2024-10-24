36e04c4d001edf0ee34f27f326a373a225179309
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-10-24 22:48:58 +0600

    hint: just check even, odd

---
+++ A_Sakurako_and_Kosuke.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int n;
+    cin >> n;
+
+    if(n&1){
+        cout << "Kosuke" << endl;
+    }else{
+        cout << "Sakurako" << endl;
+    }
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    tc{
+        solve();
+    }
+
+    return 0;
+}
