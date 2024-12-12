592212925a26a80510f89924f4fce6a2eb634ed0
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-12-12 16:20:41 +0600

    hint: just look at the difference of given numbers

---
+++ 800 Rated/A_Minimize.cpp
@@ -0,0 +1,28 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+
+    cout << (b - a) << endl;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    tc{
+        solve();
+    }
+
+    return 0;
+}
