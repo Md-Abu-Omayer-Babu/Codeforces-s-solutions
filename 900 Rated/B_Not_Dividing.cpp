c7833fa5cb03b7910ac4fcd62be9092aa1a35340
Omayer <165560324+Md-Abu-Omayer-Babu@users.noreply.github.com>
2024-12-12 16:22:32 +0600

    hint: check elements(if 1 or divisibly)

---
+++ 900 Rated/B_Not_Dividing.cpp
@@ -0,0 +1,48 @@
+// In the name of Almighty Allah
+// This code is written by "Md Abu Omayer Babu"
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define tc int t;cin >> t;while(t--)
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(auto &it : v){
+        cin >> it;
+        if(it == 1){
+            it = 2;
+        }
+    }
+
+    for(int i = 0; i < n - 1; i++){
+        if(v[i + 1] % v[i] == 0){
+            if(v[i + 1] % v[i] == 0){
+                v[i + 1] = v[i + 1] + 1;
+            }
+        }
+    }
+
+    for(auto &it : v){
+        cout << it << " ";
+    }
+    cout << endl;
+
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    tc{
+        solve();
+    }
+
+    return 0;
+}
